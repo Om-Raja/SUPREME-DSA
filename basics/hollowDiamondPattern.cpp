0414f4a00a391e17d66d80dc9ebb24bb57f0bc78
Om Raja <omraja451@gmail.com>
2024-01-22 23:57:32 +0530

    add hollowDiamondPattern.cpp

---
+++ basics/hollowDiamondPattern.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+
+    // full pyramid
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= (n - i); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            if (j == 1 || j == i)
+            cout << "* ";
+            else
+            cout << "  ";
+        }
+        cout << endl;
+    }
+    // inverted full pyramid
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= (n - i); j++)
+        {
+            if (j == 1 || j == (n - i))
+            cout << "* ";
+            else
+            cout << "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
